0ac2111f4f9b721b1743d219abd8b5c18eac94b2
deemoprobe <deemoprobe@gmail.com>
2020-09-22 15:55:23 +0800

    header php

--- header.php
+++ header.php
@@ -1,10 +1,12 @@
+<?php
 /**
  * @Author: deemoprobe@gmail.com
  * @Date:   2020-09-22 15:43:56
  * @Last Modified by:   deemoprobe@gmail.com
- * @Last Modified time: 2020-09-22 15:46:18
+ * @Last Modified time: 2020-09-22 15:52:08
  */
- <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head profile="http://gmpg.org/xfn/11">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
